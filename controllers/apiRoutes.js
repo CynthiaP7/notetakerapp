0c0f5c30f1c76aebfd0a4b0ceeb0af1814a2ce7d
Cynthia P <cynthiads417@gmail.com>
2023-10-15 15:58:53 -0400

    finish api routes

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -18,10 +18,29 @@ router.post('/notes', (req, res) => {
         if (err) throw err;
         const notes = JSON.parse(data);
 
+        let lastNoteId = notes.length > 0 ? notes[notes.length - 1].id : 0;
+        newNote.id = typeof lastNoteId === 'number' ? lastNoteId + 1 : 1;
+        notes.push(newNote);
 
+        fs.writeFile(dbPath, JSON.stringify(notes, null, 2,), (err) => {
+            if (err) throw err;
+            res.json(newNote);
+        });
     });
 });
 
-router.delete('/notes/:id'), (req, res) => {
-    
-}
+router.delete('/notes/:id', (req, res) => {
+    const noteId = parseInt(req.params.id);
+    fs.readFile(dbPath, 'utf8', (err,data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id !== noteId);
+        fs.writeFile(dbPath, JSON.stringify(notes, null, 2), (err) => {
+            if (err) throw err;
+            res.status(200).end();
+        });
+        
+    });
+});
+
+module.exports = router;
