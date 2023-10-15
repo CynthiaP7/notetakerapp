7ff390e1e62af69ed318f5850a6e33005e0425e7
Cynthia P <cynthiads417@gmail.com>
2023-10-15 15:46:56 -0400

    add controllers

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -5,8 +5,23 @@ const fs = require('fs');
 
 
 router.get('/notes', (req, res) => {
-    fs.readFile
-    if (err) throw err;
-    res.json(JSON.parse(data));
-
+    fs.readFile(dbPath, 'utf8', (err,data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
 });
+
+router.post('/notes', (req, res) => {
+    const newNote = req.body;
+
+    fs.readFile(dbPath, 'utf8', (err,data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+
+
+    });
+});
+
+router.delete('/notes/:id'), (req, res) => {
+    
+}
